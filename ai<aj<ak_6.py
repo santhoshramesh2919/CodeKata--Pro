1930b40b45a20503c9cc52596719b96ddc816ec4
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-02-15 15:34:27 +0530

    Update ai<aj<ak_6.py

--- ai<aj<ak_6.py
+++ ai<aj<ak_6.py
@@ -1,3 +1,4 @@
+#ai<aj<ak
 n=int(input())
 count=0
 a=list(map(int,input().split()))
@@ -5,7 +6,6 @@ for i in range(0,len(a)):
   for j in range(i+1,len(a)):
     for k in range(j+1,len(a)):
       if a[i]<a[j] and a[j]<a[k]:
-        print(a[i],a[j],a[k])
         count+=1
         break
 print(count)
