9c478f483a834a96dcfac0063222ba79ab2d060d
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-02-21 19:55:32 +0530

    Create sum_m_2_n_12.py

---
+++ sum_m_2_n_12.py
@@ -0,0 +1,13 @@
+#sum from m to n
+def sumof(li,m,n):
+  s=0
+  for i in range(m,n+1):
+    s=s+li[i]
+  return(s)
+n,q=map(int,input().split())
+li=list(map(int,input().split()))
+li2=[]
+for i in range(q):
+  li2.append(list(map(int,input().split())))
+for i in range(len(li2)):
+  print(sumof(li,li2[i][0]-1,li2[i][1]-1))
