0c02885071662ba1e68f7e73a2a91ebeec67c353
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-14 13:35:10 +0530

    Create Sweet_candies_16.py

---
+++ Sweet_candies_16.py
@@ -0,0 +1,13 @@
+#candies
+n=int(input())
+k=list(map(int,input().split()))
+s=[]
+s=[1]*n
+for i in range(n):
+    if i==0:
+        if k[i]>k[i+1]:
+            s[i]=s[i]+s[i+1]
+    elif i>0:
+        if k[i]>k[i-1]:
+            s[i]=s[i]+s[i-1]
+print(sum(s))
