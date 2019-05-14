ac7b08275e4db7192fad8ba3f149a2d439653e96
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-14 14:00:07 +0530

    Create AlternatingArr_51.py

---
+++ AlternatingArr_51.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+li=list(map(int,input().split()))
+op=[]
+for j in range(len(li)):
+  c=1
+  for i in range(j,len(li)-1):
+    if (li[i]>0 and li[i+1]<0) or (li[i]<0 and li[i+1]>0):
+      c+=1
+    else:
+      break
+  op.append(c)
+
+print(op)
