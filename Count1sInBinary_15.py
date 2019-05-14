fb0e6ad482bd899b48269f6575f0031075cbe199
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-14 11:38:59 +0530

    Create Count1sInBinary_15.py

---
+++ Count1sInBinary_15.py
@@ -0,0 +1,16 @@
+def bitCount(n):
+  count=0
+  while n!=0:
+    if n%2==1:
+      count+=1
+    n=n//2
+  return count
+
+n=int(input())
+li=list(map(int,input().split(" ")))
+dic={}
+for i in li:
+  dic[i]=bitCount(i)
+dic=sorted(dic,reverse=True)
+for i in dic:
+  print(i)
