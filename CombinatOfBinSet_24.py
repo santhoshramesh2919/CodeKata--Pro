93f2ca2d9c2b37d96e109c60a06428aa55883dfa
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-15 21:27:50 +0530

    Update CombinatOfBinSet_24.py

--- CombinatOfBinSet_24.py
+++ CombinatOfBinSet_24.py
@@ -3,5 +3,9 @@ from itertools import product
 n=int(input())
 li=list(map(list,product([0,1],repeat=n)))
 for i in li:
-    print(*i)
-
+    for j in range(n):
+        if j==n-1:
+            print(i[j])
+        else:
+            print(i[j],end="")
+        
