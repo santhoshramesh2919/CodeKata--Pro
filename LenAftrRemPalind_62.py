c24444110473ca59c45eeb04fb23dfa188a6201f
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-16 22:23:05 +0530

    Create LenAftrRemPalind_62.py

---
+++ LenAftrRemPalind_62.py
@@ -0,0 +1,14 @@
+s=input()
+subst=""
+f=0
+op=[]
+if s==s[::-1]:
+  op.append(0)
+for i in range(len(s)-1):
+  for j in range(i+1,len(s)):
+    subst=s[i:j+1]
+    if subst==subst[::-1]:
+      op.append(len(s)-len(subst))
+     
+print(min(op))
+  
