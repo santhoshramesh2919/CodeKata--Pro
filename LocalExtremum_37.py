3153fa63688fab217fc19c8f39accd27cce7bb68
Santhosh Kumar R <46440736+santhoshramesh2919@users.noreply.github.com>
2019-05-25 09:22:39 +0530

    Update LocalExtremum_37.py

--- LocalExtremum_37.py
+++ LocalExtremum_37.py
@@ -1,11 +1,7 @@
 n=int(input())
 li=list(map(int,input().split()))
 extr=0
-for i in range(len(li)):
-  if li[0]==li[len(li)-1]:
-    break
-  if i==0 or i==len(li)-1:
-    extr+=1
-  elif (li[i]<li[i-1] and li[i]<li[i+1]) or (li[i]>li[i-1] and li[i]>li[i+1]):
+for i in range(1,len(li)-1):
+  if (li[i]<li[i-1] and li[i]<li[i+1]) or (li[i]>li[i-1] and li[i]>li[i+1]):
     extr+=1
 print(extr)
